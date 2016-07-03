4f1676b2e2ec1a5c2f53ba838c6e2b3be9ae773e
HerrR <rbergel@kth.se>
2016-07-03 02:08:37 +0200

    Final Commit

---
+++ CS3242-Assignment02/CS3242-Assignment02/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS3242-Assignment02
+//
+//  Created by Rickard Bergeling on 21/03/16.
+//  Copyright © 2016 Rickard Bergeling. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
